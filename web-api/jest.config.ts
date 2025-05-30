56d117f37451fdf6a7ad795eecfd6d0b0173f251
Ethan Lemke <ethan@mfbtech.com>
2025-05-30 17:46:46 +0000

    test: add Jest configuration and TypeScript settings for testing

---
+++ web-api/jest.config.ts
@@ -0,0 +1,21 @@
+/**
+ * @jest-environment node
+ */
+
+import { pathsToModuleNameMapper } from "ts-jest";
+
+export default {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: pathsToModuleNameMapper({}, { prefix: "<rootDir>/" }),
+  transform: {
+    "^.+\\.m?[tj]sx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+        tsconfig: "tsconfig.test.json",
+      },
+    ],
+  },
+};
